89eb00dc241f74c26936411caa9c997f85a79b17
Keeroin <51744860+Keeroin@users.noreply.github.com>
2020-06-23 11:22:32 +0300

    Added connection to server

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Managers/GameSettings")]
+public class GameSettings : ScriptableObject
+{
+    [SerializeField] private string _gameVersion = "0.0.0";
+    public string GameVersion { get => _gameVersion; }
+
+    [SerializeField] private string _nickName = "Keeroin";
+    public string NickName { get => _nickName + Random.Range(0, 9999).ToString(); }
+}
